c49fbb1aef52067cb8e5c7b158e2d1d2ceb92dc8
Xianguang-Zhou <xianguang.zhou@outlook.com>
2018-01-11 16:31:58 +0800

    add license header

--- toolchain/windows_xp-i686.cmake
+++ toolchain/windows_xp-i686.cmake
@@ -1,3 +1,9 @@
+# Copyright (c) 2017, 2018, Xianguang Zhou <xianguang.zhou@outlook.com>. All rights reserved.
+# 
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
 list(APPEND CMAKE_MODULE_PATH "${CMAKE_CURRENT_SOURCE_DIR}/toolchain")
 include(windows-i686)
 add_definitions(-D_WIN32_WINNT=0x0501)
